fa83c7379a8af5d9484283b4d8daed96975a5cde
徐磊 <lei.xul@tongdun.net>
2020-07-24 02:16:27 +0800

    栈和队列

---
+++ Queue/src/ArrayQueue.java
@@ -0,0 +1,43 @@
+/**
+ * 用数组实现的顺序队列（非循环队列实现）
+ *
+ * @author xulei
+ * @date 2020/7/24 1:26 上午
+ */
+public class ArrayQueue {
+
+    private final String[] item;
+    /** 头指针 */
+    private int head;
+    /** 尾指针 */
+    private int tail;
+
+    public ArrayQueue(int capacity) {
+        item = new String[capacity];
+    }
+
+    /**
+     * 入队到队尾
+     */
+    public boolean enqueue(String s) {
+        if (tail == item.length) {
+            return false;
+        }
+        item[tail] = s;
+        ++tail;
+        return true;
+    }
+
+    /**
+     * 从队头出队
+     */
+    public String dequeue() {
+        // 如果队头和队尾重合，则队列中没有元素
+        if (head == tail) {
+            return null;
+        }
+        String value = item[head];
+        ++head;
+        return value;
+    }
+}
---
+++ Stack/src/ArrayStack.java
@@ -0,0 +1,35 @@
+/**
+ * @author xulei
+ * @date 2020/7/24 2:08 上午
+ */
+public class ArrayStack {
+
+    private final String[] item;
+    /** 栈顶指针 */
+    private int count;
+
+    public ArrayStack(int capacity) {
+        item = new String[capacity];
+        count = 0;
+    }
+
+    public boolean push(String s) {
+        // 栈满
+        if (count == item.length) {
+            return false;
+        }
+        item[count] = s;
+        ++count;
+        return true;
+    }
+
+    public String pop() {
+        // 栈空
+        if (count == 0) {
+            return null;
+        }
+        String value = item[count - 1];
+        --count;
+        return value;
+    }
+}
