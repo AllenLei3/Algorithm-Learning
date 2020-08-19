5d8cc777d5085ad6d9c5cb46c2fab0ce628413c2
徐磊 <17610230296@163.com>
2020-08-19 14:15:44 +0800

    动态规划——杨辉三角

--- Algorithm/src/dynamic/MatrixMinDistV2.java
+++ Algorithm/src/dynamic/MatrixMinDistV2.java
@@ -6,18 +6,18 @@ package dynamic;
  * @author xulei
  * @date 2020/8/18 2:53 下午
  */
-public class ChessboardV2 {
+public class MatrixMinDistV2 {
 
     /** 4*4的矩阵 */
-    private static final int[][] chessboard = new int[4][4];
+    private static final int[][] matrix = new int[4][4];
     /** 备忘录 */
     private static final int[][] mem = new int[4][4];
 
     static {
-        chessboard[0] = new int[]{1, 3, 5, 9};
-        chessboard[1] = new int[]{2, 1, 3, 4};
-        chessboard[2] = new int[]{5, 2, 6, 7};
-        chessboard[3] = new int[]{6, 8, 4, 3};
+        matrix[0] = new int[]{1, 3, 5, 9};
+        matrix[1] = new int[]{2, 1, 3, 4};
+        matrix[2] = new int[]{5, 2, 6, 7};
+        matrix[3] = new int[]{6, 8, 4, 3};
     }
 
     /**
@@ -28,7 +28,7 @@ public class ChessboardV2 {
      */
     public static int minDist(int i, int j) {
         if (i == 0 && j == 0) {
-            return chessboard[0][0];
+            return matrix[0][0];
         }
         // 如果备忘录里有数据，直接返回备忘录数据
         if (mem[i][j] > 0) {
@@ -43,12 +43,12 @@ public class ChessboardV2 {
         if (i - 1 >= 0) {
             minUp = minDist(i - 1, j);
         }
-        int currentDist = chessboard[i][j] + Math.min(minLeft, minUp);
+        int currentDist = matrix[i][j] + Math.min(minLeft, minUp);
         mem[i][j] = currentDist;
         return currentDist;
     }
 
     public static void main(String[] args) {
-        System.out.println(ChessboardV2.minDist(3, 3));
+        System.out.println(MatrixMinDistV2.minDist(3, 3));
     }
 }
